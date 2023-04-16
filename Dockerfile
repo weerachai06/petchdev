bde1949516fb86a6cb637d4159e1cf069560c0ff
Weerachai Plodkaew <37935229+weerachai06@users.noreply.github.com>
2023-04-16 21:04:31 +0700

    recieve args

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
 
 FROM node:18-alpine AS base
 
+ARG database_url
+ARG nextauth_secret
+ARG nextauth_url
+ARG discord_client_id
+ARG discord_client_secret
+
 # Check https://github.com/nodejs/docker-node/tree/b4117f9333da4138b03a546ec926ef50a31506c3#nodealpine to understand why libc6-compat might be needed.
 RUN apk add --no-cache libc6-compat
 RUN npm install -g pnpm
@@ -13,7 +19,11 @@ RUN pnpm build
 
 FROM base as deploy
 WORKDIR /usr/src/app
-
+ENV DATABASE_URL=$database_url
+ENV NEXTAUTH_SECRET=$nextauth_secret
+ENV NEXTAUTH_URL=$nextauth_url
+ENV DISCORD_CLIENT_ID=$discord_client_id
+ENV DISCORD_CLIENT_SECRET=$discord_client_secret
 ENV NODE_ENV "production"
 ENV PORT 3000
 COPY --from=builder /usr/src/app/ .
--- src/env.mjs
+++ src/env.mjs
@@ -5,7 +5,7 @@ import { z } from 'zod'
  * built with invalid env vars.
  */
 const server = z.object({
-  DATABASE_URL: z.string().url().optional(),
+  DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(['development', 'test', 'production']),
   // NEXTAUTH_SECRET:
   //   process.env.NODE_ENV === "production"
@@ -17,10 +17,10 @@ const server = z.object({
     (str) => process.env.VERCEL_URL ?? str,
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
     process.env.VERCEL ? z.string().min(1) : z.string().url()
-  ).optional(),
+  ),
   // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-  DISCORD_CLIENT_ID: z.string().optional(),
-  DISCORD_CLIENT_SECRET: z.string().optional(),
+  DISCORD_CLIENT_ID: z.string(),
+  DISCORD_CLIENT_SECRET: z.string(),
 })
 
 /**
